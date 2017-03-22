6f8e476166d1c9ff643b3eb8b4ce16697709c42a
Kouta_Oishi <ddssdss4455@gmail.com>
2017-03-22 09:52:33 +0900

    初期UP

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public Transform planetTransform; //惑星objectをInspectorから代入
+	float playerPosFix;
+
+	void Start() {
+		//惑星の大きさに合わせて位置を補正する変数
+		playerPosFix = planetTransform.localScale.x / 2 + 0.5f;
+	}
+		
+	void Update() {
+		//AS←→入力で角度を変更
+		transform.Rotate (0,0,-Input.GetAxis ("Horizontal"));
+
+		//三角関数を使用して角度方向で現在位置を求める
+		this.transform.position = new Vector3(-Mathf.Sin (this.transform.eulerAngles.z * Mathf.Deg2Rad) * playerPosFix
+			,Mathf.Cos (this.transform.eulerAngles.z * Mathf.Deg2Rad) * playerPosFix
+			,0);
+	}
+}
