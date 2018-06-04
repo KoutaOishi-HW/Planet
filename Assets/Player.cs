bbca17f1493be0dcef2f5552061b6c00b6b331af
MYNET\k.oishi <kouta.oishi455@gmail.com>
2018-06-04 15:27:47 +0900

    Revert "Pos名前変更"
    This reverts commit 4a4985612c49f329c622fe002755390033100b91.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,11 +2,11 @@
 
 public class Player : MonoBehaviour {
 	public Transform planetTransform; //惑星objectをInspectorから代入
-	float playerPos;
+	float playerPosFix;
 
 	void Start() {
 		//惑星の大きさに合わせて位置を補正する変数
-		playerPos = planetTransform.localScale.x / 2 + 0.5f;
+		playerPosFix = planetTransform.localScale.x / 2 + 0.5f;
 	}
 		
 	void Update() {
@@ -14,8 +14,8 @@ public class Player : MonoBehaviour {
 		transform.Rotate (0,0,-Input.GetAxis ("Horizontal"));
 
 		//三角関数を使用して角度方向で現在位置を求める
-		this.transform.position = new Vector3(-Mathf.Sin (this.transform.eulerAngles.z * Mathf.Deg2Rad) * playerPos
-			,Mathf.Cos (this.transform.eulerAngles.z * Mathf.Deg2Rad) * playerPos
+		this.transform.position = new Vector3(-Mathf.Sin (this.transform.eulerAngles.z * Mathf.Deg2Rad) * playerPosFix
+			,Mathf.Cos (this.transform.eulerAngles.z * Mathf.Deg2Rad) * playerPosFix
 			,0);
 	}
 }
