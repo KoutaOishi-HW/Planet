f410ca30f2fedcc0f2d913344bcf045eb90ad8f1
MYNET\k.oishi <kouta.oishi455@gmail.com>
2018-06-20 15:51:42 +0900

    Test

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,8 +3,9 @@
 public class Player : MonoBehaviour {
 	public Transform planetTransform; //惑星objectをInspectorから代入
 	float playerPosFix;
+    float playerPosJ;
 
-	void Start() {
+    void Start() {
 		//惑星の大きさに合わせて位置を補正する変数
 		playerPosFix = planetTransform.localScale.x / 2 + 0.5f;
 	}
@@ -15,7 +16,16 @@ public class Player : MonoBehaviour {
 
 		//三角関数を使用して角度方向で現在位置を求める
 		this.transform.position = new Vector3(-Mathf.Sin (this.transform.eulerAngles.z * Mathf.Deg2Rad) * playerPosFix
-			,Mathf.Cos (this.transform.eulerAngles.z * Mathf.Deg2Rad) * playerPosFix
-			,0);
-	}
+			,Mathf.Cos (this.transform.eulerAngles.z * Mathf.Deg2Rad) * playerPosFix + playerPosJ
+            , 0);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            playerPosJ = 0.5f;
+        }
+        else
+        {
+
+        }
+    }
 }
